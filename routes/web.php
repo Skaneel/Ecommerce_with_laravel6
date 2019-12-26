b03785aae9e34f0a85a23b629594e91a4cfbf21f
aleksandr <aleksandr1104nk@gmail.com>
2019-12-26 22:16:45 +0200

    2 С

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,19 @@
 |
 */
 
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
 });
+
+Route::get('/categories', function () {
+    return view('categories');
+});
+
+Route::get('/product', function () {
+    return view('product');
+});
+
